badb0955c4e5f6c6128689d180a176f433406d95
Arikaton <99rsw99@mail.ru>
2018-11-20 14:25:03 +0300

    perfect work

--- lab4/lab4_2.py
+++ lab4/lab4_2.py
@@ -5,6 +5,7 @@ import sympy as sp
 import re
 
 
+# класс массивов, похожий на array в NumPy
 class array:
     def __init__(self, a):
         self.a = a
@@ -14,6 +15,7 @@ class array:
         except:
             self.shape = (1, len(self.a))
 
+    # транспонирование
     def T(self):
         if self.shape[0] == 1:
             res = [[self.a[i]] for i in range(self.shape[1])]
@@ -28,6 +30,7 @@ class array:
         self.shape = (self.shape[1], self.shape[0])
         return array(res)
 
+    # умножение
     def dot(self, any):
         if any.shape[1] == 1 and self.shape[0] != 1:
             res = [[0] for i in range(any.shape[0])]
@@ -48,6 +51,7 @@ class array:
         self.shape = (self.shape[0], any.shape[1])
         return array(res)
 
+    # срез
     def slice(self, i, j):
         res = [[0 for i in range(self.shape[0] - 1)] for j in range(self.shape[1] - 1)]
         for _ in range(self.shape[0]):
@@ -62,6 +66,7 @@ class array:
                     res[_ - 1][k - 1] = self.a[_][k]
         return array(res)
 
+    # определитель
     def det(self):
         if self.shape[0] == self.shape[1]:
             if self.shape[0] == 1:
@@ -76,12 +81,15 @@ class array:
         else:
             print("Матрица не квадратная")
 
+    # алгебраическое дополнение
     def alg(self, i, j):
         return (-1) ** (i + j) * self.slice(i, j).det()
 
+    # обратная матрица
     def inv(self):
         return array([[self.alg(i, j) for i in range(self.shape[0])] for j in range(self.shape[1])]) * (1 / self.det())
 
+    # перегрузка операторов \/
     def __str__(self):
         return str(self.a)
 
@@ -98,6 +106,7 @@ class array:
         return array([[self.a[i][j] - any.a[i][j] for i in range(self.shape[0])] for j in range(self.shape[1])])
 
 
+# еденичная матрица
 def ones(size=2, value=1):
     c = [[0 for i in range(size)] for j in range(size)]
     for i in range(size):
@@ -107,6 +116,7 @@ def ones(size=2, value=1):
     return array(c)
 
 
+# функция рисует график уравнения
 def paint(a, b, c, l, r):
     f = []
     for i in range(l, r):
@@ -120,62 +130,89 @@ def paint(a, b, c, l, r):
     plt.show()
 
 
+# решение квадратного уравнения
 def solve_square(a):
     a = re.findall(r'\(.*\)', a)[0]
     a = re.sub(r'\s+', '', a)
     A, _, B, C = re.findall(r'[-+]?[ ]?\d+', a)
     A, B, C = int(A), int(B), int(C)
-    D = int(B)**2 - 4*int(A)*int(C)
-    x1, x2 = (-B+D**0.5)/(2*A), (-B-D**0.5)/(2*A)
+    D = int(B) ** 2 - 4 * int(A) * int(C)
+    x1, x2 = (-B + D ** 0.5) / (2 * A), (-B - D ** 0.5) / (2 * A)
     return x2, x1
 
 
+# определяет знак числа
 def sgn(x):
     return 1 if x >= 0 else -1
 
 
+# решает кубическое уравнение методом виетта
 def solve_cube(b):
-     b = re.findall(r'\(.*\)', b)[0]
-     b = re.sub(r'\s+', '', b)
-     _, A, __, B, C = re.findall(r'[-+]?[ ]?\d+', b)
-     A, B, C = int(A), int(B), int(C)
-     Q = (A**2-3*B)/9
-     R = (2*A**3-9*A*B+27*C)/54
-     S = Q**3-R**2
-     if S > 0:
-         ph = (math.acos(R/(Q**(3/2))))/3
-         x1 = -2*(Q**0.5)*math.cos(ph)-A/3
-         x2 = -2*(Q**0.5)*math.cos(ph + 2*math.pi/3) - A/3
-         x3 = -2*(Q**0.5)*math.cos(ph - 2*math.pi/3) - A/3
-     elif S < 0:
-         ph = math.acosh(abs(R)/(Q**(3/2)))/3
-         x1 = -2*sgn(R)*abs(Q)**0.5*math.cosh(ph) - A/3
-         x2 = complex(sgn(R)*abs(Q)**0.5*math.cosh(ph) - A/3, (3*abs(Q))**0.5*math.sinh(ph))
-         x3 = complex(sgn(R)*abs(Q)**0.5*math.cosh(ph) - A/3, -(3*abs(Q))**0.5*math.sinh(ph))
-     else:
-         x1 = -2*math.pow(R, 1/3) - A/3
-         x2 = x3 = math.pow(R, 1/3) - A/3
-     return x1, x2, x3
+    b = re.findall(r'\(.*\)', b)[0]
+    b = re.sub(r'\s+', '', b)
+    _, A, __, B, C = re.findall(r'[-+]?[ ]?\d+', b)
+    A, B, C = int(A), int(B), int(C)
+    Q = (A ** 2 - 3 * B) / 9
+    R = (2 * A ** 3 - 9 * A * B + 27 * C) / 54
+    S = Q ** 3 - R ** 2
+    if S > 0:
+        ph = (math.acos(R / (Q ** (3 / 2)))) / 3
+        x1 = -2 * (Q ** 0.5) * math.cos(ph) - A / 3
+        x2 = -2 * (Q ** 0.5) * math.cos(ph + 2 * math.pi / 3) - A / 3
+        x3 = -2 * (Q ** 0.5) * math.cos(ph - 2 * math.pi / 3) - A / 3
+    elif S < 0:
+        ph = math.acosh(abs(R) / (Q ** (3 / 2))) / 3
+        x1 = -2 * sgn(R) * abs(Q) ** 0.5 * math.cosh(ph) - A / 3
+        x2 = complex(sgn(R) * abs(Q) ** 0.5 * math.cosh(ph) - A / 3, (3 * abs(Q)) ** 0.5 * math.sinh(ph))
+        x3 = complex(sgn(R) * abs(Q) ** 0.5 * math.cosh(ph) - A / 3, -(3 * abs(Q)) ** 0.5 * math.sinh(ph))
+    else:
+        x1 = -2 * math.pow(R, 1 / 3) - A / 3
+        x2 = x3 = math.pow(R, 1 / 3) - A / 3
+    return x1, x2, x3
 
 
+# массивы со случайными значениями
 A = array([[randint(0, 10) for i in range(3)] for j in range(3)])  # 3x3
 B = array([[randint(0, 10)] for i in range(3)])  # 3x1
 C = array([randint(0, 10) for i in range(3)])  # 1x3
 
-
 if __name__ == '__main__':
-    x = sp.symbols('s')
-    res = sp.factor(C.dot((ones(3, x) - A).inv()).dot(B)[0])
+    x = sp.symbols('s')  # инициализация символьной переменной
+    res = sp.factor(C.dot((ones(3, x) - A).inv()).dot(B)[0])  # решение уравнения
     pattern = r'.*\(.*\)\/'
     a = re.findall(pattern, str(res))
-    a = a[0][:-1]
+    a = a[0][:-1]       # числитель уравнения
+    print(a)
     b = str(res)
-    b = b[len(a)+1:]
+    b = b[len(a) + 1:]  # знаменатель уравнения
     print("Уравнение:", res)
     dec = solve_square(a), solve_cube(b)
-    print('Корни числителя: {0}; {1}\nКорни знаменателя: {2}; {3}; {4}'.format(dec[0][0], dec[0][1], dec[1][0], dec[1][1], dec[1][2]))
+    print(
+        'Корни числителя: {0}; {1}\nКорни знаменателя: {2}; {3}; {4}'.format(dec[0][0], dec[0][1], dec[1][0], dec[1][1],
+                                                                             dec[1][2]), '\n')
 
+    flag = True
+    for i in range(len(dec[0])):
+        for j in range(len(dec[1])):
+            if dec[0][i] == dec[1][j]:
+                print('проблемы с управляемостью/наблюдаемостью')
+                flag = False
+                break
+    if flag:
+        print('нет проблемы с управляемостью/наблюдаемостью')
 
-
-
-
+    # проверка на устойчивость
+    zero = False
+    flag = True
+    for i in range(3):
+        if dec[1][i].real > 0:
+            print('система неустойчива')
+            flag = False
+            break
+        elif dec[1][i].real == 0:
+            zero += True
+    if flag:
+        if zero:
+            print('система находится на границе устойчивости')
+        else:
+            print('система устойчива')
